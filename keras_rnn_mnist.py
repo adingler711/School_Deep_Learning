155b147854f8d71d61da1603e1c14b2c8cbf24fa
Andy Dingler <andy.dingler@comcast.net>
2017-06-03 14:31:32 -0700

    updated rnn

--- keras_rnn_mnist.py
+++ keras_rnn_mnist.py
@@ -15,13 +15,9 @@ img_rows, img_cols = 28, 28
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# transpose the variables
-x_train = x_train.reshape(x_train.shape[0], -1, 1)
-x_test = x_test.reshape(x_test.shape[0], -1, 1)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
@@ -31,7 +27,9 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(LSTM(32, input_shape=x_train.shape[1:]))
+model.add(LSTM(100, input_shape=x_train.shape[1:], return_sequences=True))
+model.add(LSTM(500, return_sequences=True))
+model.add(LSTM(10))
 
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
